e130cdb93f8008f4d8eba28c31cf1781753a87f8
hungrykirby <katsuki8hanawa6@gmail.com>
2020-04-26 22:51:02 +0900

    最初のランディングだけLoad画面を表示したい

--- components/loading.vue
+++ components/loading.vue
@@ -1,49 +1,41 @@
 <template lang="html">
-  <div style="display: none;" class="loading-page" v-bind:class="{'loading': loading }">
-    <p>Loading...</p>
+  <div style="" class="loading-page" v-if="loading">
+    <!--<p>Loading...</p>-->
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    loading: false
+    loading: false,
   }),
   methods: {
     start () {
-      this.loading = true
+      // サイト内遷移の場合はfirstLandingがundefinedになる
+      if(this.firstLanding !== 0){
+        this.loading = true
+      }
     },
     finish () {
+      this.firstLanding = 0 // 一回でもサイト上でロードしていると（サイト内遷移以外なら0を入れる）
       this.loading = false
     }
   }
 }
 </script>
 
-<style scoped>
-@keyframes slideInFromLeft {
-  0% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
-    visibility: hidden;
-  }
-}
+<style>
 .loading-page {
-  animation: 0.5s slideInFromLeft;
-}
-.loading-page.loading {
-  /* animation: 0.5s slideInFromLeft; */
-  position: fixed !important;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
   background: rgba(255, 255, 255, 0.8);
+  color: black;
   text-align: center;
   padding-top: 200px;
   font-size: 30px;
-  z-index: 10000000000000000000000;
+  z-index: 10000000000000000000;
 }
 </style>
