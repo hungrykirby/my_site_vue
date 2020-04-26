ab1332e90ad60f32a37afdcd04644058bb56f191
hungrykirby <katsuki8hanawa6@gmail.com>
2020-04-26 23:00:42 +0900

    日本語

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,9 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     titleTemplate: '%s - きゃりかつルーム',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
